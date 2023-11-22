3ed2fd3c6a1d7cc7c37e93938932fba6d9a8130a
gd-harco <gd-harco@student.42lyon.fr>
2023-11-22 16:02:39 +0100

    added more test in main.cpp

--- Module_05/ex00/incs/header.hpp
+++ Module_05/ex00/incs/header.hpp
@@ -13,4 +13,14 @@
 #ifndef HEADER_HPP
 #define HEADER_HPP
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define EXC_CAUGHT "Exception caught: "
+
 #endif //HEADER_HPP
--- Module_05/ex00/srcs/main.cpp
+++ Module_05/ex00/srcs/main.cpp
@@ -10,21 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "header.hpp"
 #include "Bureaucrat.hpp"
 int main() {
-	Bureaucrat *good;
-	Bureaucrat *tooLow;
+	{
+		Bureaucrat *good = NULL;
+		Bureaucrat *tooLow = NULL;
 
-	try {
-		good = new Bureaucrat("good", 5);
-		std::cout << *good << std::endl;
-		tooLow = new Bureaucrat("bad", 175);
+		try {
+			good = new Bureaucrat("good", 5);
+			std::cout << *good << std::endl;
+			tooLow = new Bureaucrat("bad", 175);
+			good->incrementGrade();
+			std::cout << good << std::endl;
+			tooLow->decrementGrade();
+		} catch (std::exception &e) {
+			std::cout << RED << EXC_CAUGHT << e.what() << RESET << std::endl;
+		}
+		std::cout << "Incrementing Bureaucrat\"good\"" << std::endl;
 		good->incrementGrade();
-		std::cout << good << std::endl;
-		tooLow->decrementGrade();
+		std::cout << *good << std::endl;
+		std::cout << "decrementing Bureaucrat\"good\"" << std::endl;
+		good->decrementGrade();
+		std::cout << *good << std::endl;
+		delete good;
 	}
-	catch (std::exception & e) {
-		std::cout << "coucou Benoit " << e.what() << std::endl;
+	{
+		Bureaucrat *level1 = new Bureaucrat("level one", 1);
+		Bureaucrat &level1Ref = *level1;
+		std::cout << level1Ref << std::endl;
+		try {
+			level1Ref.decrementGrade();
+			std::cout << level1Ref << std::endl;
+			level1Ref.incrementGrade();
+			std::cout << level1Ref << std::endl;
+			level1Ref.incrementGrade();
+			std::cout << level1Ref << std::endl;
+		}
+		catch (std::exception & e) {
+			std::cout << RED << EXC_CAUGHT << e.what() << RESET << std::endl;
+		}
+		delete (level1);
 	}
 	return 0;
 }
