ae4a81d223318e01f6f75cd47679db4d24a54950
gd-harco <gd-harco@student.42lyon.fr>
2023-11-28 11:39:38 +0100

    removed old todo

--- Module_06/ex00/srcs/main.cpp
+++ Module_06/ex00/srcs/main.cpp
@@ -6,13 +6,12 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:15:11 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/11/10 15:01:26 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/11/28 11:39:16 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
-//TODO:handle -inff +inff
 int main(int argc, char **argv) {
 	if (argc != 2)
 		return (std::cout << "usage: ./convert <std::string>litteral" << std::endl, 1);
