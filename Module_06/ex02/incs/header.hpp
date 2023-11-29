6204f716696945f8d5f2ed6f9498aaebbcf31a26
gd-harco <gd-harco@student.42lyon.fr>
2023-11-29 16:25:02 +0100

    resolve ex02

---
+++ Module_06/ex02/incs/header.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 10:25:32 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/11/09 16:25:56 by gd-harco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <iostream>
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#endif //HEADER_HPP
---
+++ Module_06/ex02/srcs/main.cpp
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 17:15:11 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/11/10 15:01:26 by gd-harco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <typeinfo>
+
+#include "header.hpp"
+
+void setSeed();
+Base *generate();
+static std::string	getInput();
+void identify(Base* p);
+void identify(Base& p);
+
+int main() {
+	setSeed();
+	std::string		input;
+	int				temp;
+
+	std::cout << "number of test to Generate?" << std::endl;
+	temp = std::atoi(getInput().c_str());
+	while (temp <= 0) {
+		std::cout << "Please enter a positive value" << std::endl;
+		temp = std::atoi(getInput().c_str());
+	}
+
+	//generate the array of class
+	Base *testArray[temp];
+	for (int i=0; i<temp; i++)
+		testArray[i] = generate();
+	//Execute both identify function on every member of the array;
+	for (int i=0; i<temp; i++){
+		identify(testArray[i]);
+		identify(*testArray[i]);
+	}
+	//Properly free the array.
+	for (int i=0; i<temp; i++)
+		delete testArray[i];
+}
+
+void setSeed() {
+	const std::time_t time = std::time(0);
+	std::tm *tm = std::localtime(&time);
+	std::srand((tm->tm_hour + tm->tm_mon) / (tm->tm_sec + 1));
+}
+
+Base *generate() {
+	switch (std::rand() % 3)
+	{
+		case(1): return new B;
+		case(2): return new C;
+		default: return new A;
+	}
+	return (NULL);
+}
+
+static std::string	getInput() {
+	std::string input;
+
+	std::getline(std::cin, input);
+	if (!std::cin)
+	{
+		std::cout << "STOP CLOSIN CIN YOU IDIOT!" << std::endl;
+		return NULL;
+	}
+	return (input);
+}
+
+void	identify(Base* p) {
+	const A *ATest = dynamic_cast<A*>(p);
+	const B *BTest = dynamic_cast<B*>(p);
+	const C *CTest = dynamic_cast<C*>(p);
+
+	if (ATest)
+		std::cout << "Pointer to " << p << " is of type A" << std::endl;
+	else if (BTest)
+		std::cout << "Pointer to " << p << " is of type B" << std::endl;
+	else if (CTest)
+		std::cout << "Pointer to " << p << " is of type C" << std::endl;
+	else
+		std::cout << "Strange Situation" << std::endl;
+}
+
+void	identify(Base& p) {
+	try {
+		dynamic_cast<A&>(p);
+		std::cout << "Ref to " << &p << " is of type A" << std::endl;
+	} catch (std::bad_cast &a) {
+		try {
+			dynamic_cast<B&>(p);
+			std::cout << "Ref to " << &p << " is of type B" << std::endl;
+		} catch (std::bad_cast &b) {
+			try {
+				dynamic_cast<C&>(p);
+				std::cout << "Ref to " << &p << " is of type C" << std::endl;
+			} catch (std::bad_cast &c) {
+				std::cout << "Strange Situation" << std::endl;
+			}
+		}
+	}
+}
