95d2b5fc8404513930752309017e9d6f02183699
gd-harco <gd-harco@student.42lyon.fr>
2023-12-01 17:46:11 +0100

    fixed template folder

--- template/srcs/main.cpp
+++ template/srcs/main.cpp
@@ -6,42 +6,11 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:15:11 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/11/10 15:01:26 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/12/01 17:42:43 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "header.hpp"
-
-LMateria	*g_List;
-
-//Never alocate Materias on the stack, it will cause a crash when freeing the list.
+#include "header.hpp".
 
 int main(void) {
-	Character	*Cloud = new Character("Cloud");
-	Character	*Aerith = new Character("Aerith");
-	g_List = new LMateria;
-	g_List->current = NULL;
-	g_List->next = NULL;
-
-	AMateria	*iceSword = new Ice();
-	AMateria	*healStick = new Cure();
-	AMateria	*iceGauntlet = new Ice();
-	AMateria	*healthPotion = new Cure();
-	// AMateria	*iceArrow = new Ice();
-	// AMateria	*chocobo = new Cure();
-
-	Cloud->equip(iceSword);
-	Cloud->equip(healStick);
-	Cloud->equip(iceGauntlet);
-	Cloud->equip(healthPotion);
-	//TODO: Copy assignment operator never called, see why
-	// Aerith = Cloud;
-	Aerith->equip(Cloud->getInventory(1)->clone());
-	Aerith->use(1, *Cloud);
-	Aerith->unequip(1);
-	Aerith->unequip(2);
-	Aerith->unequip(3);
-	LMateriaFree();
-	delete Cloud;
-	delete Aerith;
 }
