750a80107c211314807601674245c0fcc54b8a38
gd-harco <gd-harco@student.42lyon.fr>
2023-12-05 20:49:34 +0100

    Start work on Array template

--- Module_07/ex02/incs/Array.hpp
+++ Module_07/ex02/incs/Array.hpp
@@ -13,4 +13,22 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+template <typename T>
+class Array{
+private:
+	T*				_array;
+	unsigned int	_size;
+public:
+	Array() :
+			_array(new T[0]),
+			_size(0)
+	{};
+	Array(const unsigned int len) :
+			_array(new T[len]),
+			_size(len)
+	{};
+
+};
+
+
 #endif //ARRAY_HPP
