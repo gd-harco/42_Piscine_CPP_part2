ef18fd8de2d8f3a68d36ea330be14492b3e7d1a4
gd-harco <gd-harco@student.42lyon.fr>
2023-12-05 20:15:24 +0100

    fixed min max functions

--- Module_07/ex00/incs/template.hpp
+++ Module_07/ex00/incs/template.hpp
@@ -12,11 +12,11 @@ void	swap(T &a, T &b){
 
 template <typename T>
 T		min(T const &a, const T &b){
-	return (a > b)? a: b;
+	return (a > b)? b: a;
 }
 
 template <typename T>
 T	max(T const &a, T const  &b){
-	return (a > b)? b: a;
+	return (a > b)? a: b;
 }
 #endif //TEMPLATE_HPP
