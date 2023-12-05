df60041ff6554319a0838f3a34cbeb5eec564eeb
gd-harco <gd-harco@student.42lyon.fr>
2023-12-05 15:57:09 +0100

    solved ex01 ?

---
+++ Module_07/ex01/incs/iter.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 10:25:32 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/11/09 16:25:56 by gd-harco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template <class T, class U>
+void iter(T *array, int len, U(*function)(T)){
+	for (int i=0; i<len; i++)
+		array[i] = function(array[i]);
+}
+
+#endif //ITER_HPP
---
+++ Module_07/ex01/srcs/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 17:15:11 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/12/01 17:42:43 by gd-harco         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+#include <string>
+#include <iostream>
+
+int	incr(int i);
+void printArray(int *array, int len);
+
+int main(void) {
+	int (*incFunc)(int);
+	incFunc = incr;
+	int *array = new int[8];
+	for (int i=0; i<7; i++)
+		array[i] = i+1;
+	array[7] = 0;
+	printArray(array, 8);
+	iter(array, 8, incFunc);
+	std::cout << "----------ITER----------" << std::endl;
+	printArray(array, 8);
+	delete[] array;
+}
+
+int	incr(int i){
+	return i + 1;
+}
+
+void printArray(int *array, int len){
+	for (int i=0; i<len; i++)
+		std::cout << array[i] << std::endl;
+}
