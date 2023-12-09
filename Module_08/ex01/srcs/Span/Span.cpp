e6dbd169cbb82868fe18d968e09406499b641409
gd-harco <gd-harco@student.42lyon.fr>
2023-12-09 13:53:12 +0100

    (Module 08 ex01) Finished Orthodox Canonical function in Span class

--- Module_08/ex01/srcs/Span/Span.cpp
+++ Module_08/ex01/srcs/Span/Span.cpp
@@ -3,30 +3,23 @@
 //
 
 #include "Span.hpp"
-#include "algorithm"
 
 Span::Span()
-		: _spanSize(0), _storage(new int[_spanSize]){}
+		: _spanSize(0), _storage(std::vector<int>(0)){}
 
 Span::Span(unsigned int spanSize)
-		: _spanSize(spanSize), _storage(new int[_spanSize]){}
+		: _spanSize(spanSize), _storage(std::vector<int>(_spanSize)){}
 
-Span::~Span() {
-	delete[] _storage;
-}
+Span::~Span() {}
 
 Span& Span::operator=(const Span& rhs) {
 	if (this == &rhs)
 		return *this;
-	if (this->_spanSize != rhs._spanSize) {
-		std::cout << "Can't assign a Span to another one with different span size" << std::endl;
-		return *this;
-	}
-	std::copy(rhs._storage, rhs._storage + _spanSize, this->_storage);
+	this->_spanSize = rhs._spanSize;
+	this->_storage = rhs._storage;
 	return *this;
 }
 
-Span::Span(const Span& old)
-		: _spanSize(old._spanSize), _storage(new int[_spanSize]) {
+Span::Span(const Span& old) {
 	*this = old;
 }
--- Module_08/ex01/srcs/Span/Span.hpp
+++ Module_08/ex01/srcs/Span/Span.hpp
@@ -5,7 +5,6 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
-#include <iostream>
 #include <vector>
 
 class Span {
