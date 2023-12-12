ca64c0faa9b783c80e61a90ea8da53670efb1f4f
gd-harco <gd-harco@student.42lyon.fr>
2023-12-12 15:08:03 +0100

    (Module 08 ex01) Finish test in main.cpp

--- Module_08/ex01/srcs/main.cpp
+++ Module_08/ex01/srcs/main.cpp
@@ -12,16 +12,11 @@
 
 #include "Span.hpp"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-//TODO: Add Number
-//TODO: failure when span already full
-//TODO: batch add number in span
-//TODO: failure if the resulting span would be bigger than authorized
-//TODO: test shortestPast
-//TODO: test longestSpan;
-//TODO: exception is empty span or of size 1;
-//TODO: big span.
 int main(){
+	std::srand(std::time(NULL));
 //testing small Span
 	{
 		Span smallSpan(5);
@@ -35,6 +30,31 @@ int main(){
 		} catch (std::exception &e){
 			std::cout << e.what() << std::endl;
 		}
-
+		std::cout << "Shortest Span in given data: " << smallSpan.shortestSpan() << std::endl;
+		std::cout << "Longest Span in given data: " << smallSpan.longestSpan() << std::endl;
+	}
+//test on big Span
+	{
+		Span bigSpan(20000);
+		std::vector<int> big(19999) ;
+		std::generate(big.begin(), big.end(), std::rand);
+		try {
+			bigSpan.addRange(big.begin(), big.end()); //prove batch add on range iterator for std::vector
+		} catch (std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << "Shortest Span in given data: " << bigSpan.shortestSpan() << std::endl;
+		std::cout << "Longest Span in given data: " << bigSpan.longestSpan() << std::endl;
+	}
+//proving that batchAdd also throw an exception
+	{
+		int array[20];
+		Span tooSmall(10);
+		std::generate(array, array + 20, std::rand);
+		try {
+			tooSmall.addRange(array, array + 20);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
 }
